4300a3cad83ece9b43e8e98227a47be1b7dd88cb
Atiya Afzal <atiyajhg@gmail.com>
2025-11-09 20:05:42 +0500

    staff file

---
+++ Staff/StaffFunctions.java
@@ -0,0 +1,10 @@
+package com.example.Lodgify.Staff;
+
+public abstract class StaffFunctions {
+
+    // abstract method
+
+   public abstract void showDataForOne();
+   public abstract void updateStatus();
+    
+}
